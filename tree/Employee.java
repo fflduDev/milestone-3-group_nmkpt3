c4e2ef1089f432dce4c4a8b307e8b7e32fdc2720
kevinjt2001 <kjtorrealba@gmail.com>
2025-03-26 14:45:07 -0400

    Added constructor method to Employee class

--- tree/Employee.java
+++ tree/Employee.java
@@ -1,40 +1,40 @@
 package tree;
 
-
 /**
  * The entity we're storing in our node
  */
 
 public class Employee implements Comparable<Employee> {
-	
+
 	private String name;
 	private Integer id;
 	private String position;
-	
- 	public String getName() {
-		return name;
+
+	public Employee(String name, Integer id, String position) {
+		this.name = name;
+		this.id = id;
+		this.position = position;
 	}
 
+	public String getName() {
+		return name;
+	}
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
 	public Integer getId() {
 		return id;
 	}
-
 
 	public void setId(Integer id) {
 		this.id = id;
 	}
 
-
 	public String getPosition() {
 		return position;
 	}
-
 
 	public void setPosition(String position) {
 		this.position = position;
@@ -49,11 +49,11 @@ public class Employee implements Comparable<Employee> {
 	public String toString() {
 		return name + " " + id + " " + position;
 	}
-	
+
 	//
 	public int compareTo(Employee o) {
-		
+
 		return (this.id).compareTo(o.id);
-	 
+
 	}
 }
