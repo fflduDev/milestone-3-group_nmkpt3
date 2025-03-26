019a7f48401d084b661a285f5f4156bda3933ad3
kevinjt2001 <kjtorrealba@gmail.com>
2025-03-26 15:05:20 -0400

    Added methods, finished addRoot and clear

--- tree/OrgChartImpl.java
+++ tree/OrgChartImpl.java
@@ -3,10 +3,47 @@ package tree;
 import java.util.ArrayList;
 import java.util.List;
 
-public class OrgChartImpl implements OrgChart{
+public class OrgChartImpl implements OrgChart {
 
-	//Employee is your generic 'E'..
+	// Employee is your generic 'E'..
 	private List<GenericTreeNode<Employee>> nodes = new ArrayList<>();
-	
-	
+	private GenericTreeNode<Employee> root;
+
+	@Override
+	public void addRoot(Employee e) {
+		root = new GenericTreeNode<Employee>(e);
+		nodes.add(root);
+	}
+
+	@Override
+	public void clear() {
+		nodes = new ArrayList<>();
+		root = null;
+
+	}
+
+	@Override
+	public void addDirectReport(Employee manager, Employee newPerson) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeEmployee(Employee firedPerson) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showOrgChartDepthFirst() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showOrgChartBreadthFirst() {
+		// TODO Auto-generated method stub
+
+	}
+
 }
