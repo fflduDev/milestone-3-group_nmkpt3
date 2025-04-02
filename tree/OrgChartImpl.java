f9806f0c5f5029b10ca196a3937a34ee635207cc
kevinjt2001 <kjtorrealba@gmail.com>
2025-04-02 15:26:17 -0400

    Updated BreadthFirst method

--- tree/OrgChartImpl.java
+++ tree/OrgChartImpl.java
@@ -1,11 +1,9 @@
 package tree;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-import java.util.Set;
 
 public class OrgChartImpl implements OrgChart {
 
@@ -15,7 +13,7 @@ public class OrgChartImpl implements OrgChart {
 
 	@Override
 	public void addRoot(Employee e) {
-		if(root== null) {
+		if (root == null) {
 			GenericTreeNode<Employee> root = new GenericTreeNode<Employee>(e);
 			nodes.add(root);
 		}
@@ -31,9 +29,9 @@ public class OrgChartImpl implements OrgChart {
 	@Override
 	public void addDirectReport(Employee manager, Employee newPerson) {
 		// TODO Auto-generated method stub
-		for(int i = 0; i< nodes.size(); i++) {
+		for (int i = 0; i < nodes.size(); i++) {
 			GenericTreeNode<Employee> currentEmployee = nodes.get(i);
-			if(currentEmployee.data.equals(manager)) {
+			if (currentEmployee.data.equals(manager)) {
 				GenericTreeNode<Employee> newNode = new GenericTreeNode<Employee>(newPerson);
 				currentEmployee.addChild(newNode);
 				nodes.add(newNode);
@@ -55,32 +53,30 @@ public class OrgChartImpl implements OrgChart {
 
 	}
 
-	
 	@Override
 	public void showOrgChartBreadthFirst() {
-		if(root == null) {
+		if (root == null) {
 			return;
 		}
-		
-		Queue<GenericTreeNode> q = new LinkedList<>();
-		q.add(root);
-		
-		for(int i = 0; i < nodes.size(); i++) {
-			while (!q.isEmpty()) {
-				int n = q.size();
 
-				// If this node has children
-				while (n > 0) {
-					// Dequeue an item from queue and print it
-					TreeNode p = q.peek();
-					q.remove();
-					System.out.print(p.key + " ");
+		Queue<GenericTreeNode<Employee>> queue = new LinkedList<>();
+		ArrayList<Employee> explored = new ArrayList<>();
+
+		for (int i = 0; i < nodes.size(); i++) {
+			queue.add(nodes.get(i));
+			while (!queue.isEmpty()) {
+				GenericTreeNode<Employee> current = queue.remove();
+
+				if (!explored.contains(current.data)) {
+					explored.add(current.data);
 				}
+				if (current.children.size() > 0) {
+					queue.addAll(current.children);
+				}
+
 			}
 		}
-            
-        
-
+		for (Employee e : explored)
+			System.out.println(e.toString());
 	}
-
 }
