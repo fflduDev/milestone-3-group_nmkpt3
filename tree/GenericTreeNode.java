0050c110e12c0e397e6209498f9eba48fce03b67
Nazli Yaglidere <nazli.yaglidere@student.fairfield.edu>
2025-04-10 10:45:30 -0400

    mariella's updates

--- tree/GenericTreeNode.java
+++ tree/GenericTreeNode.java
@@ -18,7 +18,16 @@ public class GenericTreeNode<E> {
 		// this one is a little harder.
 		// what do you do when the item has children?
 		// I suggest "give them to the parent"
-
+	       for (int i = 0; i < children.size(); i++) {
+	           GenericTreeNode<E> child = children.get(i);
+	           if (child.data.equals(theItem)) {
+	               // Promote its children to this node
+	               children.addAll(child.children);
+	               // Remove the child
+	               children.remove(i);
+	               return;
+	           }
+	       }
 		
 	}
 	
